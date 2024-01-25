fdb4eb9e4fffa4d7e30ba41718e636dc00bf8b67
Reed Searle <csearle@cnm.edu>
2024-01-25 14:42:54 -0700

    Added classes as well as shuffling and sorting on deck

--- src/main/java/edu/cnm/deepdive/cards/Main.java
+++ src/main/java/edu/cnm/deepdive/cards/Main.java
@@ -1,8 +1,22 @@
 package edu.cnm.deepdive.cards;
 
+import edu.cnm.deepdive.cards.model.Deck;
+import java.security.SecureRandom;
+import java.util.zip.Deflater;
+
 public class Main {
 
   public static void main(String[] args) {
+    Deck deck = new Deck();
+    System.out.println(deck);
+
+    deck.shuffle(new SecureRandom());
+    System.out.println(deck);
+
+    deck.sort();
+    System.out.println(deck);
+
+
     // TODO: 1/25/2024 Execute card trick 
   }
 }
